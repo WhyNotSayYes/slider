d916301ed07bae4e3f4c08af1a6b5eea50bd10f3
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-06-23 23:21:37 +0300

    initial

---
+++ slider-js.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.display').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        dots: false,
+        draggable: false,
+        swipe: false,
+        autoplay: true,
+    });
+});
+
